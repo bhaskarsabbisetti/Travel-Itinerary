ecd097ded3cfad7e478f467e8ba9ea8dd12fb355
bhaskarsabbisetti <bhaskarsabbisetti@gmail.com>
2025-12-04 09:07:49 +0530

    finally done

--- travel-itinerary-cr-with-backend/backend/accounts/urls_auth.py
+++ travel-itinerary-cr-with-backend/backend/accounts/urls_auth.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views_auth
 
 urlpatterns = [
-    path('register', views_auth.register, name='register'),
-    path('login', views_auth.login, name='login'),
-    path('logout', views_auth.logout, name='logout'),
-    path('me', views_auth.me, name='me'),
+    path('register/', views_auth.register, name='register'),
+    path('login/', views_auth.login, name='login'),
+    path('logout/', views_auth.logout, name='logout'),
+    path('me/', views_auth.me, name='me'),
 ]
